48058bc7f3e84dffa6115022d20b868e57c561e3
Bnroberts3755 <54412579+Bnroberts3755@users.noreply.github.com>
2019-09-17 12:23:29 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 # Brandon Roberts
 # Space trivia
 
-print("Welcome to the epitome of space trivia!")
+print ("Welcome to the epitome of space trivia!")
+name = input("Enter your name: ")
+print ("Hello", name, "I hope you are ready to begin!")
+print ("The following is a series of questions to help you determine just how much you may know about space.")
+print (2-2)
+### Figure out how to keep program from closing  
