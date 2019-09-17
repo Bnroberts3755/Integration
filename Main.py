054267128636bdf1f258e12c95f58ba56812fca1
Bnroberts3755 <brandon.n.roberts@gmail.com>
2019-09-17 12:38:21 -0400

    Configuring interpreter

--- Main.py
+++ Main.py
@@ -6,4 +6,6 @@ name = input("Enter your name: ")
 print ("Hello", name, "I hope you are ready to begin!")
 print ("The following is a series of questions to help you determine just how much you may know about space.")
 print (2-2)
-### Figure out how to keep program from closing  
+print ("Hello")
+### Figure out how to keep program from closing
+
